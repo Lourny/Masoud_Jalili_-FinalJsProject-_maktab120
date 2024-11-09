36d8591b9237c2d759fb4ca92a815f5f0935f9e8
masoud_jalili <masoudjalili0781@gmail.com>
2024-11-09 23:10:42 +0330

    complete loading page of welcom(logo & spiner)

--- src/components/button.js
+++ src/components/button.js
@@ -1,9 +1,9 @@
 import {ce} from "../Utils/create-element.js";
 
-export default function btn() {
+export default function btn(innerTxt) {
     const btn = ce("button", {
-        innerText: "test",
-        className: "bg-slate-400 px-3 py-2 rounded-md text-white w-[120px] font-bold text-xl hover:bg-slate-600"
+        innerText: innerTxt,
+        className: "bg-slate-400 px-3 py-2 rounded-md text-white w-[120px] font-bold text-xl hover:bg-slate-600",
     });
     return btn;
 }
--- src/layout/layout.js
+++
@@ -1,9 +0,0 @@
-import {ce} from "../Utils/create-element.js";
-import Header from "./header.js";
-export default function layout(){
-    const layout=ce("div",{
-        children:[Header()],
-        className:"w-full"
-    })
-    return layout;
-}
