3bd4195c88ddd6a62c3f54675ca139ca9a1292f6
masoud_jalili <masoudjalili0781@gmail.com>
2024-11-19 17:24:21 +0330

    comlete 60% project for present

--- src/api/addidas.api.js
+++ src/api/addidas.api.js
@@ -1,3 +1,5 @@
+import clog from "../Utils/logdata.js";
+
 export default async function getAdiddasProductsList(endpoint){
     const response=await fetch(`http://localhost:5173/products?brand=${endpoint}`);
     const data= await response.json();
--- src/pages/fetchAdiddasProducts.js
+++ src/pages/fetchAdiddasProducts.js
@@ -1,19 +1,17 @@
-
-import {ce} from "../Utils/create-element.js";
+import { ce } from "../Utils/create-element.js";
 import cardElement from "../components/card.js";
 import getAdiddasProductsList from "../api/addidas.api.js";
-
+import clog from "../Utils/logdata.js";
 
 export default function fetchAdiddasProducts(endpoint) {
-    let home=ce("div",{
-        className:"w-full pt-6 flex flex-wrap  justify-center items-center gap-4"
+  let home = ce("div", {
+    className: "w-full pt-6 flex flex-wrap  justify-center items-center gap-4",
+  });
+  getAdiddasProductsList(endpoint).then((products) => {
+    products.forEach((product) => {
+      let card = cardElement(product);
+      home.appendChild(card);
     });
-    getAdiddasProductsList(endpoint).then(products=>{
-        products.forEach(product=>{
-            let card=cardElement(product);
-            home.appendChild(card);
-        })
-    })
-    return home;
+  });
+  return home;
 }
-
