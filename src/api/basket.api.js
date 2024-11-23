facd5932ba4e825dcdb4a32573a913fa65f5f816
masoud_jalili <masoudjalili0781@gmail.com>
2024-11-23 17:06:40 +0330

    show confirmation to user for delete data

--- src/api/basket.api.js
+++ src/api/basket.api.js
@@ -1,8 +1,13 @@
 import clog from "../Utils/logdata.js";
 
-
-export default async function fetchFromBasket() {
+export default async function fetchFromBasket(endpoint = "") {
+  if (endpoint != "") {
+    let card = await fetch(`http://localhost:5173/basket/${endpoint}`);
+    let res = await card.json();
+    return res;
+  } else {
     let card = await fetch(`http://localhost:5173/basket`);
     let res = await card.json();
     return res;
+  }
 }
