f4318dfce5dcf6dfb9a31964976b51e7608f4752
masoud_jalili <masoudjalili0781@gmail.com>
2024-11-25 11:27:33 +0330

    comlete the shipping component in checkoutPage

--- src/pages/checkout/checkout.js
+++ src/pages/checkout/checkout.js
@@ -1,11 +1,12 @@
 import header from "../../components/checkout/checkout-header";
+import shipingNav from "../../components/checkout/checkout-shipping";
 import { ce } from "../../Utils/create-element";
 import clog from "../../Utils/logdata";
 
 export default function checkout() {
   let checkout2 = ce("div", {
     className: "w-full h-screen bg-pink-600 flex flex-col px-6",
-    children: [header("checkout", true)],
+    children: [header("checkout", true), shipingNav()],
   });
   return checkout2;
 }
