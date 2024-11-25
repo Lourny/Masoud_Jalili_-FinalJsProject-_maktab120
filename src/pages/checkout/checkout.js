cd414578a200f2b5ce0d0c79bf61cc36cdefeae2
masoud_jalili <masoudjalili0781@gmail.com>
2024-11-25 13:29:52 +0330

    comlete the order list component

--- src/pages/checkout/checkout.js
+++ src/pages/checkout/checkout.js
@@ -1,4 +1,5 @@
 import header from "../../components/checkout/checkout-header";
+import orderList from "../../components/checkout/checkout-orderList";
 import shipingNav from "../../components/checkout/checkout-shipping";
 import { ce } from "../../Utils/create-element";
 import clog from "../../Utils/logdata";
@@ -6,7 +7,7 @@ import clog from "../../Utils/logdata";
 export default function checkout() {
   let checkout2 = ce("div", {
     className: "w-full h-screen bg-pink-600 flex flex-col px-6",
-    children: [header("checkout", true), shipingNav()],
+    children: [header("checkout", true), shipingNav(), orderList()],
   });
   return checkout2;
 }
