ca15b3f8536b363f7a386ae239beb8673d426240
masoud_jalili <masoudjalili0781@gmail.com>
2024-11-26 22:51:35 +0330

    complete before searchbox

--- src/components/input.js
+++ src/components/input.js
@@ -3,7 +3,10 @@ import {ce} from "../Utils/create-element.js";
 export default function input(){
     let input=ce("input",{
         className:"pl-0 pr-3 py-2 bg-[#FAFAFA] rounded border-none focus:outline-0 w-11/12 ",
-        restAttrs:{placeholder:"Search"}
+        restAttrs:{placeholder:"Search"},
+        events:{
+
+        }
     })
     return input;
 }
