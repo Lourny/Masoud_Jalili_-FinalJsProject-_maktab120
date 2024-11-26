0d3d2c359ffda2321bc642455d07bdd9577b7c2a
masoud_jalili <masoudjalili0781@gmail.com>
2024-11-26 23:36:52 +0330

    setup search page and route it

---
+++ src/pages/search/searchPage.js
@@ -0,0 +1,9 @@
+import { ce } from "./../../Utils/create-element";
+
+
+export default function searchPage() {
+  let search = ce("div", {
+    className: "w-full h-screen bg-yellow-400",
+  });
+  return search;
+}
